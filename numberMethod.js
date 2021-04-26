2878ec2ff13a1f98582be4dffd9ff30c32ab5b41
MostafizurRahman199 <67595254+MostafizurRahman199@users.noreply.github.com>
2021-04-26 23:46:14 +0600

    Add files via upload

---
+++ numberMethod.js
@@ -0,0 +1,12 @@
+
+var num = 3.1416;
+
+console.log(num.toFixed(5));
+console.log(num.toPrecision(5));
+console.log(typeof(Number("12")));
+console.log(Number("12.5484"));
+console.log(Number(true));
+console.log(Number(false));
+console.log(num.toFixed(6))
+console.log(num.toPrecision(3));
+console.log(num.toFixed(4));
